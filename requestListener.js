71e49024509c466e9547fee257dc1fd75fb4d909
tenterrain <153330685@qq.com>
2016-03-20 17:34:04 +0800

    fix bug

--- requestListener.js
+++ requestListener.js
@@ -7,57 +7,66 @@ let zlib = require('zlib');
 let mime = require('./mime.json');
 let config = require('./config.json');
 
-module.exports = exports = (req, res) => {
-    let pathname = url.parse(req.url).pathname;
-    let realPath = 'static' + pathname;
+let pathHandle = (realPath, req, res) => {
 
-    fs.exists(realPath, isExists => {
-        if (isExists) {
-            fs.stat(realPath, (err, stats) => {
-                if (err) {
-                    res.writeHead(500, 'Internal Server Error', { 'Content-Type': 'text/plain' });
-                    res.end(err.stack);
-                } else {
-                    let lastModified = new Date(stats.mtime);
-                    let ifModifiedSince = new Date(req.headers['if-modified-since']);
-
-                    res.setHeader('Last-Modified', lastModified.toUTCString());
-                    if (ifModifiedSince &&
-                        lastModified.getTime() <= ifModifiedSince.getTime()) {
-                        res.writeHead(304, 'Not Modified', { 'Content-Type': 'text/plain' });
-                        res.end();
-                    } else {
-                        let ext = path.extname(realPath);
-                        ext = ext ? ext.slice(1) : 'unknow';
-
-                        if (new RegExp(config['expiresMatch'], 'i').test(ext)) {
-                        	let expires = new Date();
-                        	expires.setTime(expires.getTime() + config['maxAge'] * 1000);
-
-                        	res.setHeader('Expires', expires.toUTCString());
-                        	res.setHeader('Cache-Control', 'max-age=' + config['maxAge']);
-                        }
-
-                        let compressMatch = new RegExp(config['compressMatch'], 'i').test(ext);
-                        let acceptEncoding = req.headers['accept-encoding'];
-                        let fileStream = fs.createReadStream(realPath);
-
-                        if (compressMatch && new RegExp('\\bgzip\\b', 'i').test(acceptEncoding)) {
-                            res.writeHead(200, 'Ok', { 'Content-Encoding': 'gzip' });
-                            fileStream.pipe(zlib.createGzip()).pipe(res);
-                        } else if (compressMatch && new RegExp('\\bdeflate\\b', 'i').test(acceptEncoding)) {
-                            res.writeHead(200, 'Ok', { 'Content-Encoding': 'deflate' });
-                            fileStream.pipe(zlib.createDeflate()).pipe(res);
-                        } else {
-                            res.write(200, 'Ok', { 'Content-Type': 'text/plain' });
-                            fileStream.pipe(res);
-                        }
-                    }
-                }
-            });
-        } else {
+    fs.stat(realPath, (err, stats) => {
+        if (err) {
             res.writeHead(404, 'Not Found', { 'Content-Type': 'text/plain' });
             res.end('the request URL ' + pathname + ' was not found on this server!');
+        } else {
+            if (stats.isDirectory()) {
+            	pathHandle(path.join(realPath, '/', config['homePageFile']), req, res);
+            } else {
+                let lastModified = new Date(stats.mtime);
+                let ifModifiedSince = new Date(req.headers['if-modified-since']);
+
+                res.setHeader('Last-Modified', lastModified.toUTCString());
+                if (ifModifiedSince &&
+                    lastModified.getTime() <= ifModifiedSince.getTime()) {
+                    res.writeHead(304, 'Not Modified', { 'Content-Type': 'text/plain' });
+                    res.end();
+                } else {
+                    let ext = path.extname(realPath);
+                    ext = ext ? ext.slice(1) : 'unknow';
+                    res.setHeader('Content-Type', mime[ext] || 'text/plain');
+
+                    if (new RegExp(config['expiresMatch'], 'i').test(ext)) {
+                        let expires = new Date();
+                        expires.setTime(expires.getTime() + config['maxAge'] * 1000);
+
+                        res.setHeader('Expires', expires.toUTCString());
+                        res.setHeader('Cache-Control', 'max-age=' + config['maxAge']);
+                    }
+
+                    let compressMatch = new RegExp(config['compressMatch'], 'i').test(ext);
+                    let acceptEncoding = req.headers['accept-encoding'];
+                    let fileStream = fs.createReadStream(realPath);
+
+                    if (compressMatch && new RegExp('\\bgzip\\b', 'i').test(acceptEncoding)) {
+                        res.writeHead(200, 'Ok', { 'Content-Encoding': 'gzip' });
+                        fileStream.pipe(zlib.createGzip()).pipe(res);
+                    } else if (compressMatch && new RegExp('\\bdeflate\\b', 'i').test(acceptEncoding)) {
+                        res.writeHead(200, 'Ok', { 'Content-Encoding': 'deflate' });
+                        fileStream.pipe(zlib.createDeflate()).pipe(res);
+                    } else {
+                        res.writeHead(200, 'Ok');
+                        fileStream.pipe(res);
+                    }
+                }
+            }
         }
     });
+}
+
+module.exports = exports = (req, res) => {
+    let pathname = url.parse(req.url).pathname;
+    if (pathname[pathname.length - 1] === '/') {
+        pathname += config['homePageFile'];
+    }
+    //禁止父路径
+    let realPath = path.join('static', path.normalize(pathname.replace(/\.\./g, '')));
+
+    res.setHeader('Server', 'Node/5.8.0');
+    pathHandle(realPath, req, res);
+
 };
